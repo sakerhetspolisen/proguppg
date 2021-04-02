4ed3754ae097834b9dcb1613d8b7996b49e47016
Karl Sellergren <karl.sellergren@partikular.se>
2021-04-02 11:31:43 +0200

    Added L1

---
+++ L1/main.py
@@ -0,0 +1,25 @@
+"""
+OBS! Om du är klar med en level kan du kommentera ut den med 3st " före och efter koden så att den inte körs när du vill testa på andra levels.
+"""
+
+
+
+# Level 1
+
+radius = 5.0
+
+
+
+# Level 2
+
+number = 23
+denominator = 3
+
+
+
+# Level 3
+
+initial = 10
+hours = 5
+
+
